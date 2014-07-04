f07fa26c3d4ff14fe6e9bce8033a12296fdf5cc3
Simon Mercier <smercier@optimus.(none)>
2014-07-03 23:50:05 -0400

    add scribui

--- init_vagrant.sh
+++ init_vagrant.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo ===== Vagrant and Virtualbox =====
-wget http://files.vagrantup.com/precise64.box
-vagrant box add scribeui precise64.box
+if [ ! -f precise64.box ]
+then
+     wget http://files.vagrantup.com/precise64.box
+fi
+vagrant box add --force scribeui precise64.box
 vagrant up
 
 
--- install_msa.sh
+++ install_msa.sh
@@ -4,40 +4,43 @@ echo ===== Install utf-8 =====
 export LANGUAGE=en_CA.UTF-8
 export LANG=en_CA.UTF-8
 export LC_ALL=en_CA.UTF-8
-sudo localedef -i en_CA -f UTF-8 en_CA
-sudo locale-gen en_CA.UTF-8
-sudo update-locale
-sudo sudo dpkg-reconfigure locales
+localedef -i en_CA -f UTF-8 en_CA
+locale-gen en_CA.UTF-8
+update-locale
+dpkg-reconfigure locales
 echo ===== Vagrant Postinstal =====
-# sudo sh postinstall.sh
-sudo apt-get update
+# sh postinstall.sh
+apt-get update
 echo ===== Mapserver Suite OSM tool =====
-sudo apt-get install -y python2.7 python-software-properties
-sudo add-apt-repository -y ppa:ubuntugis/ppa
-sudo apt-get update
-sudo apt-get install -y postgresql-9.1 postgresql-server-dev-9.1 postgresql-contrib-9.1 postgis
-sudo apt-get install -y apache2 binutils checkinstall git vim screen make python-virtualenv python-pip python-all-dev osm2pgsql osmosis
-sudo apt-get install -y gdal-bin cgi-mapserver mapserver-bin libmapcache mapcache-cgi mapcache-tools libapache2-mod-mapcache tinyows
-sudo mkdir /tmp/ms_tmp
-sudo chown www-data:www-data /tmp/ms_tmp
+apt-get install -y python2.7 python-software-properties
+add-apt-repository -y ppa:ubuntugis/ppa
+apt-get update
+apt-get install -y postgresql-9.1 postgresql-server-dev-9.1 postgresql-contrib-9.1 postgresql-9.1-postgis-2.0
+apt-get install -y apache2 binutils checkinstall git vim screen make python-virtualenv python-pip python-all-dev osm2pgsql osmosis
+apt-get install -y gdal-bin cgi-mapserver mapserver-bin libmapcache mapcache-cgi mapcache-tools libapache2-mod-mapcache tinyows
+mkdir /tmp/ms_tmp
+chown www-data:www-data /tmp/ms_tmp
 
 echo ===== OSM tool =====
 wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert
-sudo mv osmconvert /usr/bin
-sudo apt-get install -y build-essential python-dev protobuf-compiler libprotobuf-dev libtokyocabinet-dev python-psycopg2 libgeos-c1
-sudo pip install imposm
+mv osmconvert /usr/bin
+apt-get install -y build-essential python-dev protobuf-compiler libprotobuf-dev libtokyocabinet-dev python-psycopg2 libgeos-c1
+pip install imposm
 
 echo ===== WSGI mod =====
-sudo wget https://modwsgi.googlecode.com/files/mod_wsgi-3.4.tar.gz
-sudo tar xzf mod_wsgi-3.4.tar.gz && cd mod_wsgi-3.4
+apt-get -y install python-dev apache2-prefork-dev
+wget https://modwsgi.googlecode.com/files/mod_wsgi-3.4.tar.gz
+tar xzf mod_wsgi-3.4.tar.gz && cd mod_wsgi-3.4
 ./configure --with-python=/usr/bin/python2.7
-sudo make
-sudo make install
-cat >/etc/apache2/mods-enabled/wsgi2.load  <<EOF 
+make
+make install
+
+cat >/etc/apache2/mods-available/wsgi.load  <<EOF 
 LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so 
 EOF
+ln -s /etc/apache2/mods-available/wsgi.load /etc/apache2/mods-enabled/wsgi.load
 
-sudo ln -s /etc/apache2/mods-enabled/wsgi.load /etc/apache2/mods-available/wsgi
+
 echo ===== init PostGIS =====
 sudo su postgres -c'createdb -E UTF8 --lc-ctype en_CA.UTF-8 -T template0 template_postgis'
 sudo su postgres -c'createlang -d template_postgis plpgsql;'
@@ -53,55 +56,49 @@ sudo su postgres -c'psql -U postgres -d osm -c "CREATE USER osm WITH PASSWORD '"
 sudo su postgres -c'psql -U postgres -d osm -c "GRANT ALL PRIVILEGES ON DATABASE osm to osm;"'
 echo ===== Configuring postgresql =====
 sed -i -e "s/#listen_addresses\ \=\ 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.1/main/postgresql.conf
-echo "host    all             all             0.0.0.0/0            md5" | tee -a /etc/postgresql/9.1/main/pg_hba.conf > /dev/null
+echo "host    all             all             0.0.0.0/0            md5" | tee -a : > /dev/null
 service postgresql restart
 echo ===== Install ScribeUI =====
 
 cd /opt
-sudo git clone https://github.com/mapgears/scribeui.git
-cd scribui
+git clone https://github.com/smercier/scribeui.git && cd scribeui
 
-PATH_=${PWD} 
-CURRENT_DIR=${PWD##*/}
+echo "Install app..."
 
-echo "creating $PATH_/pyramid.wsgi"
+sed -i -e "s/'127.0.0.1'/'127.0.0.1:8080'/g" proxy.cgi
+sed -i -e "s/localhost\/cgi-bin/localhost:8080\/cgi-bin/g" production.ini
+sed -i -e "s/mapcache.output.directory =/#mapcache.output.directory =/g" production.ini
+cp production.ini local.ini
+make
+make install
+make perms
+## make load-basescribe-data
 
-cat > $PATH_/pyramid.wsgi <<EOF
+echo "creating /opt/scribeui/pyramid.wsgi"
+
+cat > /opt/scribeui/pyramid.wsgi <<EOF
 from pyramid.paster import get_app, setup_logging
-ini_path = '$PATH_/production.ini'
+ini_path = '/opt/scribeui/production.ini'
 setup_logging(ini_path)
 application = get_app(ini_path, 'main')
 EOF
 
-echo "Install app..."
-sudo make
-sudo chown -R youruser .
-make install
-sudo make perms
+echo "Creating /etc/apache2/sites-available/scribeui"
 
-if [ ! -f /etc/apache2/sites-available/$CURRENT_DIR ]
-then
+cat > /etc/apache2/sites-available/scribeui <<EOF
+WSGIDaemonProcess scribeui user=www-data group=www-data threads=10 python-path=/opt/scribeui/lib/python2.7/site-packages
+WSGIScriptAlias /scribeui /opt/scribeui/pyramid.wsgi
 
-echo "Creating /etc/apache2/sites-available/$CURRENT_DIR"
-
-cat > /etc/apache2/sites-available/$CURRENT_DIR <<EOF
-WSGIDaemonProcess $CURRENT_DIR user=www-data group=www-data threads=10 python-path=$PATH_/lib/python2.7/site-packages
-WSGIScriptAlias /$CURRENT_DIR $PATH_/pyramid.wsgi
-
-<Directory $PATH_>
+<Directory /opt/scribeui/>
         WSGIApplicationGroup %{ENV:APPLICATION_GROUP}
         WSGIPassAuthorization On
-        WSGIProcessGroup $CURRENT_DIR
+        WSGIProcessGroup scribeui
         Order deny,allow
         Allow from all
 </Directory>
 EOF
 
-ln -s /etc/apache2/sites-available/$CURRENT_DIR /etc/apache2/sites-enabled/$CURRENT_DIR
+ln -s /etc/apache2/sites-available/scribeui /etc/apache2/sites-enabled/scribeui
 
-
-fi
-
-sudo ln -s /etc/apache2/sites-available/scribeui_pyramid /etc/apache2/sites-enabled/scribeui_pyramid
-sudo apachectl restart
+service apache2 restart
 
